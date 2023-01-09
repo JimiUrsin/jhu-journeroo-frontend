af1098c4b2eac32807b2ced77672a875665c8db8
JimiUrsin <32343914+JimiUrsin@users.noreply.github.com>
2023-01-09 17:55:37 +0200

    Fixed test

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders title', () => {
   render(<App />);
-  const linkElement = screen.getByText(/Journeroo!/i);
+  const linkElement = screen.getByText(/Journeroo/i);
   expect(linkElement).toBeInTheDocument();
 });
