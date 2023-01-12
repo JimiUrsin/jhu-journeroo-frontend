5ecd36bdafe37726bbc9a9198d0f491a68ef8f36
JimiUrsin <32343914+JimiUrsin@users.noreply.github.com>
2023-01-12 15:52:07 +0200

    Added support for sorting in Journey repo

--- src/services/journeys.js
+++ src/services/journeys.js
@@ -1,14 +1,12 @@
 import axios from 'axios'
 const baseUrl = process.env.REACT_APP_BACKEND_URL
 
-const getAll = () => {
-    console.log(`Sending GET request to ${baseUrl}/api/journeys/`)
-    return axios.get(`${baseUrl}/api/journeys/`)
+const getAll = (sort, column) => {
+    return axios.get(`${baseUrl}/api/journeys/`, {params: {sort: sort, column: column}})
 }
 
-const getPage = (page) => {
-    console.log(`Sending GET request to ${baseUrl}/api/journeys/${page}`)
-    return axios.get(`${baseUrl}/api/journeys/${page}`)
+const getPage = (page, sort, column) => {
+    return axios.get(`${baseUrl}/api/journeys/${page}`, {params: {sort: sort, column: column}})
 }
 
 export default { getAll, getPage };
